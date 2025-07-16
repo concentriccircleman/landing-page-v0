9f73d30fdc2c43ea6e13ff9a9e832052984ea62f
jaegpark <jaepark.g@gmail.com>
2025-07-16 06:21:33 -0400

    remove

--- src/app/(home)/_components/Hero.tsx
+++ src/app/(home)/_components/Hero.tsx
@@ -7,8 +7,6 @@ import HeroHeader from "./HeroHeader";
 import { ResponsiveLogoCarousel } from "./ByVariants";
 import { motion } from "motion/react";
 import { useAnimation } from "@/app/providers";
-import { cn } from "@/lib/utils";
-import AnimatedListDemo from "./AnimatedList";
 
 export default function Hero() {
   const { isLoaded } = useAnimation();
@@ -18,7 +16,7 @@ export default function Hero() {
       <HeroHeader />
 
       <div className="relative z-10 flex flex-col w-full h-full text-foreground bg-transparent justify-center items-start pointer-events-auto">
-        <div className="mt-24 mb-16 ml-32 xs:my-16 px-8 max-w-2xl h-full flex items-center">
+        <div className="mt-24 mb-16 xs:my-16 px-8 max-w-2xl h-full flex items-center">
           <div className="flex flex-col gap-6">
             <motion.h1
               className="text-3xl/tight s:text-4xl/tight md:text-5xl/tight lg:text-6xl/tight font-medium"
@@ -68,16 +66,6 @@ export default function Hero() {
             </motion.div>
           </div>
         </div>
-        {/* <div className="hidden sm:flex mt-24 mb-16 xs:my-16 px-8 max-w-5xl h-full items-center justify-center">
-          <div className="flex flex-col gap-6">
-            <AnimatedListDemo
-              className={cn(
-                "h-full min-h-[350px] max-h-[350px] sm:min-h-[500px] sm:max-h-[500px] transition-all duration-400 ease-out",
-                "group-hover:scale-105"
-              )}
-            />
-          </div>
-        </div> */}
       </div>
 
       <HeroFooter />
