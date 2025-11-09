3a126cfa2a7e16d4252f874143b9932220ad3cfd
Jae Park <47578882+jaegpark@users.noreply.github.com>
2025-11-08 23:02:17 -0500

    fix (#20)
    * fix
    
    * fix apos

--- src/app/(home)/_components/hero/index.tsx
+++ src/app/(home)/_components/hero/index.tsx
@@ -25,15 +25,14 @@ export default function Hero() {
           <div className="relative z-10 flex-1 flex items-center pt-24 pb-8 sm:py-0">
             <div className="w-full max-w-screen-4xl mx-auto px-4">
               <div className="flex flex-col gap-4 xs:gap-6 max-w-2xl">
-                <h1 className="leading-snug text-3xl s:text-4xl md:text-5xl lg:text-6xl font-medium">
-                  Stay Aligned as You Scale
+                <h1 className="leading-snug text-2xl s:text-3xl md:text-4xl lg:text-5xl font-medium">
+                Empower your enterprise with organizational memory
                 </h1>
-                <p className="text-sm xs:text-base s:text-lg md:text-xl text-background/80">
-                  Sentra is your AI alignment officer who creates a{" "}
-                  <b>unified company memory</b>, remembering details everyone
-                  forgot and alerting you when your teams are misaligned.
+                <p className="text-sm xs:text-base s:text-md md:text-lg text-background/80">
+                Sentra is your organizational memory system that transforms your company&apos;s 
+                collective knowledge, data and decisions into living intelligence. 
                 </p>
-
+                <div></div>
                 <Link
                   href="/signup"
                   className="w-fit mt-2 text-base bg-background text-foreground px-5 py-2 hover:brightness-80 duration-200 hover:cursor-pointer"
