e6d4bd6848da362030707ae710e2f7a8bdba423f
jaden-park-sentra <jaden@sentra.app>
2026-01-28 15:50:24 -0500

    add link to banner (#38)

--- src/components/announcement-banner.tsx
+++ src/components/announcement-banner.tsx
@@ -9,11 +9,16 @@ interface AnnouncementBannerProps {
 
 const AnnouncementBanner = ({ message }: AnnouncementBannerProps) => {
   return (
-    <div className={cn("fixed top-0 inset-x-0 z-50 w-full announcement-gradient text-background")}>
+    <a
+      href="https://www.linkedin.com/feed/update/urn:li:activity:7422322649269350400/"
+      target="_blank"
+      rel="noopener noreferrer"
+      className={cn("fixed top-0 inset-x-0 z-50 w-full announcement-gradient text-background cursor-pointer block")}
+    >
       <div className="max-w-screen-2xl mx-auto w-full px-4 h-10 flex items-center justify-center">
         <p className="text-xs sm:text-sm text-background text-center">{message}</p>
       </div>
-    </div>
+    </a>
   );
 };
 
